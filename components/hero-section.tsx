31c2b0a01e96d112e5570bd79ff50755f4a0b654
Nwosa <obere4u@gmail.com>
2025-05-06 18:43:00 +0100

    fix: Theme error on mobile

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -19,39 +19,39 @@ const fadeUp = {
 export default function HeroSection() {
   return (
     <AnimatePresence>
-    <motion.section
-      initial="hidden"
-      animate="visible"
-      id='about'
-      aria-label='About Tochukwu Nwosa.'
+      <motion.section
+        initial="hidden"
+        animate="visible"
+        id='about'
+        aria-label='About Tochukwu Nwosa.'
         className=" pb-5 relative pt-24 overflow-hidden"
-    >
- <GridBG/>
+      >
+        <GridBG />
         {/* bg Overlay */}
         <div className="absolute inset-0 bg-gradient-to-b !from-foreground/98 !to-foreground/98 dark:!from-background/98 dark:!to-background/98" />
 
-      {/* Hero Content */}
-        <div className="relative max-w-7xl px-4 sm:px-6 lg:px-8 z-10 mx-auto flex flex-col lg:flex-row space-y-20 lg:space-y-0 lg:justify-between">
-        {/* name and links */}
-        <motion.div
-          className="relative flex flex-col items-center lg:items-start space-y-5"
-          variants={fadeUp}
-          custom={1}
-        >
-          <motion.h1
-            className="hero-title text-center lg:text-left text-4xl sm:text-6xl md:text-7xl lg:text-7xl font-extrabold"
+        {/* Hero Content */}
+        <div className=" max-w-7xl px-4 sm:px-6 lg:px-8 z-10 mx-auto flex flex-col lg:flex-row space-y-20 lg:space-y-0 lg:justify-between">
+          {/* name and links */}
+          <motion.div
+            className=" flex flex-col items-center lg:items-start space-y-5"
             variants={fadeUp}
             custom={1}
           >
-            Tochukwu Nwosa
-          </motion.h1>
-          <motion.span
-            className='dark:text-foreground/70 text-center lg:text-left text-xl sm:text-2xl tracking-[0.015em] pl-4'
-            variants={fadeUp}
-            custom={2}
-          >
-            Full-stack Developer
-          </motion.span>
+            <motion.h1
+              className="hero-title text-center lg:text-left text-4xl sm:text-6xl md:text-7xl lg:text-7xl font-extrabold"
+              variants={fadeUp}
+              custom={1}
+            >
+              Tochukwu Nwosa
+            </motion.h1>
+            <motion.span
+              className='dark:text-foreground/70 text-center lg:text-left text-xl sm:text-2xl tracking-[0.015em] pl-4'
+              variants={fadeUp}
+              custom={2}
+            >
+              Full-stack Developer
+            </motion.span>
             <motion.div
               className="dark:text-foreground/70 text-center lg:text-left text-base sm:text-lg max-w-2xl sm:mx-0 pl-4"
               variants={fadeUp}
@@ -70,53 +70,53 @@ export default function HeroSection() {
               </motion.a>
             </motion.div>
 
-          <motion.div
-            className='grid grid-cols-2 gap-5 w-fit'
-            variants={fadeUp}
-            custom={4}
-          >
-            <motion.a
-              href="https://www.github.com/obere4u"
-              target="_blank"
-              rel="noopener noreferrer"
-              className='flex items-center justify-center cursor-pointer size-12 bg-foreground text-background dark:bg-background dark:text-foreground rounded-full shadow-lg dark:shadow-sm shadow-foreground'
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.95 }}
+            <motion.div
+              className='grid grid-cols-2 gap-5 w-fit'
+              variants={fadeUp}
+              custom={4}
             >
-              <GithubIcon />
-            </motion.a>
-            <motion.a
-              href="https://www.linkedin.com/in/nwosa-tochukwu"
-              target="_blank"
-              rel="noopener noreferrer"
-              className='flex items-center justify-center cursor-pointer size-12 bg-foreground text-background dark:bg-background dark:text-foreground rounded-full shadow-lg dark:shadow-sm shadow-foreground'
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.95 }}
-            >
-              <LinkedinIcon />
-            </motion.a>
+              <motion.a
+                href="https://www.github.com/obere4u"
+                target="_blank"
+                rel="noopener noreferrer"
+                className='flex items-center justify-center cursor-pointer size-12 bg-foreground text-background dark:bg-background dark:text-foreground rounded-full shadow-lg dark:shadow-sm shadow-foreground'
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                <GithubIcon />
+              </motion.a>
+              <motion.a
+                href="https://www.linkedin.com/in/nwosa-tochukwu"
+                target="_blank"
+                rel="noopener noreferrer"
+                className='flex items-center justify-center cursor-pointer size-12 bg-foreground text-background dark:bg-background dark:text-foreground rounded-full shadow-lg dark:shadow-sm shadow-foreground'
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.95 }}
+              >
+                <LinkedinIcon />
+              </motion.a>
+            </motion.div>
           </motion.div>
-        </motion.div>
 
-        {/* about me card */}
-        <motion.div
-          className='lg:flex-1 lg:ml-10'
-        >
+          {/* about me card */}
           <motion.div
-            variants={fadeUp}
-            custom={5}
-            className='w-full'
+            className='lg:flex-1 lg:ml-10'
           >
-            <AboutMeCard />
+            <motion.div
+              variants={fadeUp}
+              custom={5}
+              className='w-full'
+            >
+              <AboutMeCard />
+            </motion.div>
+            <motion.div
+              variants={fadeUp}
+              custom={6}>
+              <ResumeDownload />
+            </motion.div>
           </motion.div>
-          <motion.div
-            variants={fadeUp}
-            custom={6}>
-            <ResumeDownload />
-          </motion.div>
-        </motion.div>
-      </div>
-    </motion.section>
+        </div>
+      </motion.section>
     </AnimatePresence>
   )
 }
